f87f02986bf34bab5bdbf4d679ccf887da0c3de4
Guillaume <guillaume.derouville@gmail.com>
2017-10-19 16:40:07 +0200

    D04 finished ohhh yeaaahh

--- S3/D04/tictactoe.rb
+++ S3/D04/tictactoe.rb
@@ -23,7 +23,7 @@ class Board
   end
 
   def add_piece(coords, piece)
-    @board[coords[0]][coords[1]] = piece
+    	@board[coords[0]][coords[1]] = piece
   end
 
   def to_s
@@ -36,9 +36,49 @@ class Board
     #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
   end
 
-  def victory?
-    #TO DO : qui gagne ?
+  def is_available(coords)
+  	if (@board[coords[0]][coords[1]] == "X") || (@board[coords[0]][coords[1]] == "O")
+  		return false
+  	else
+  		return true
+  	end
   end
+
+  def victory?
+	 if ((@board[0][0] == "X") && (@board[0][1] == "X") && (@board[0][2] == "X")) ||
+	    ((@board[1][0] == "X") && (@board[1][1] == "X") && (@board[1][2] == "X")) ||
+	    ((@board[2][0] == "X") && (@board[2][1] == "X") && (@board[2][2] == "X")) ||
+	    ((@board[0][0] == "X") && (@board[1][0] == "X") && (@board[2][0] == "X")) ||
+	    ((@board[0][1] == "X") && (@board[1][1] == "X") && (@board[2][1] == "X")) ||
+	    ((@board[0][2] == "X") && (@board[1][2] == "X") && (@board[2][2] == "X")) ||
+	    ((@board[0][0] == "X") && (@board[1][1] == "X") && (@board[2][2] == "X")) ||
+	    ((@board[0][2] == "X") && (@board[1][1] == "X") && (@board[2][0] == "X"))
+	   puts "Player 1 is the winner!"
+	   return true
+	 elsif ((@board[0][0] == "O") && (@board[0][1] == "O") && (@board[0][2] == "O")) ||
+	    ((@board[1][0] == "O") && (@board[1][1] == "O") && (@board[1][2] == "O")) ||
+	    ((@board[2][0] == "O") && (@board[2][1] == "O") && (@board[2][2] == "O")) ||
+	    ((@board[0][0] == "O") && (@board[1][0] == "O") && (@board[2][0] == "O")) ||
+	    ((@board[0][1] == "O") && (@board[1][1] == "O") && (@board[2][1] == "O")) ||
+	    ((@board[0][2] == "O") && (@board[1][2] == "O") && (@board[2][2] == "O")) ||
+	    ((@board[0][0] == "O") && (@board[1][1] == "O") && (@board[2][2] == "O")) ||
+	    ((@board[0][2] == "O") && (@board[1][1] == "O") && (@board[2][0] == "O"))
+	   puts "Player 2 is the winner!"
+	   return true
+	 elsif ((@board[0][0] == ("X" || "O")) &&
+	    (@board[0][1] == ("X" || "O")) &&
+	    (@board[0][2] == ("X" || "O")) &&
+	    (@board[1][0] == ("X" || "O")) &&
+	    (@board[1][1] == ("X" || "O")) &&
+	    (@board[1][2] == ("X" || "O")) &&
+	    (@board[2][0] == ("X" || "O")) &&
+	    (@board[2][1] == ("X" || "O")) &&
+	    (@board[2][2] == ("X" || "O")))
+	 		#all the boxes are full	
+	   puts "Tie Game"
+	   return true
+	 end
+	end
 end
 
 class Player
@@ -65,7 +105,20 @@ class Player
 
 	def get_coordinates
   	coords = ask_for_coordinates
-		@board.add_piece(coords, @piece)
+  	if (check_coordinates(coords) == true && @board.is_available(coords) == true)
+			@board.add_piece(coords, @piece)
+		else
+			puts "try again, the coordinates you gave are not possible"
+			get_coordinates
+		end
+	end
+
+	def check_coordinates(coords)
+		if coords.length == 2 && (coords[0] <= 2 && coords[0] >= 0) && (coords[1] <= 2 && coords[1] >= 0)
+			return true
+		else 
+			return false
+		end
 	end
 end
 
@@ -82,31 +135,32 @@ class Game
   end
 
   def go
-    # TO DO : lance la partie
-    	puts "Player 1 will play the X"
-    	puts "Player 2 will play the O"
+    	puts "Player 1 will play : X"
+    	puts "Player 2 will play : O"
     	puts "-------------------------"
     	puts "\n"
     	turn
   end
 
   def turn
-  	@current_player.who_s_turn
-    
-    @game.to_s
+  	until @game.victory? == true
+	  	@current_player.who_s_turn
+	    
+	    @game.to_s
 
-    @current_player.get_coordinates
-
-    if @current_player == @player1
-        @current_player = @player2
-    else
-        @current_player = @player1
-    end
-    turn    
+	    @current_player.get_coordinates
+	    
+	    if @current_player == @player1
+	        @current_player = @player2
+	    else
+	        @current_player = @player1
+	    end
+    end  
     #@game.to_s
     #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
+    puts @game.to_s
+    puts "Game finished ;-)"
   end
-
 end
 
 a = Game.new
